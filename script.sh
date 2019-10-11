b6127d958de5a924b2ac53fa0f3941ecf095d703
Cailey Brogan <caileybrogan@Caileys-MacBook-Pro.local>
2019-10-10 20:32:42 -0400

    final script

--- script.sh
+++ script.sh
@@ -1,10 +1,19 @@
-#usage: bash script.sh inputFile outputFile
+#usage: bash script2.sh wages.csv
 
-cat wages.csv | cut -d ',' -f 1,2 | sed 's/\,/\ /' > gender-years.txt
-grep -w "male" gender-years.txt | sort -k 2n > male-years.txt
-grep -w "female" gender-years.txt | sort -k 2n > female-years.txt
-cat female-years.txt > gender-years-sort.txt
-cat male-years.txt >> gender-years-sort.txt
-rm female-years.txt
-rm male-years.txt
-rm gender-years.txt
+cat $1 | cut -d , -f 1,2 | tr ',' ' ' | sort -t ' ' -k1,1 -k2,2 -n > $1_sorted3.txt
+
+echo "highest earner"
+cat $1 | grep -v 'gender' | sort -t , -k 4 -n | tail -n 1
+
+echo "lowest earner"
+cat $1 | grep -v 'gender' | sort -t , -k 4 -n | head -n 1
+
+echo "top ten female earners"
+cat $1 | grep -v 'gender' | sort -t , -k 4 -n | tail -n 10 | grep -c 'female'
+
+echo "effect of graduating college on mimimum wage"
+VAR1=$(cat $1 | grep -E '[female|male][,][0-9]{1,4}[,]16' | sort -t , -k 4 -n | head -n 1 | cut -d , -f 4)
+VAR2=$(cat $1 | grep -E '[female|male][,][0-9]{1,4}[,]12' | sort -t, -k 4 -n | head -n 1 | cut -d , -f 4)
+echo "$VAR1-$VAR2" | bc
+
+
