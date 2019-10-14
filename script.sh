a9d60942955a229f0aebb58155ccab644ce9bca1
Cailey Brogan <caileybrogan@Caileys-MacBook-Pro.local>
2019-10-14 00:31:01 -0400

    Final Version for Submission

--- script.sh
+++ script.sh
@@ -1,15 +1,15 @@
-#usage: bash script2.sh wages.csv
+#usage: bash script.sh wages.csv
 
-cat $1 | cut -d , -f 1,2 | tr ',' ' ' | sort -t ' ' -k1,1 -k2,2 -n > $1_sorted.txt
+cat $1 | grep -v "gender" | cut -d , -f 1,2 | tr ',' ' ' | sort -k1,1d -k2,2n > sorted.txt
 
 echo "highest earner"
-cat $1 | grep -v 'gender' | sort -t , -k 4 -n | tail -n 1
+cat $1 | grep -v "gender" | sort -t , -k 4 -n | tail -n 1
 
 echo "lowest earner"
-cat $1 | grep -v 'gender' | sort -t , -k 4 -n | head -n 1
+cat $1 | grep -v "gender" | sort -t , -k 4 -n | head -n 1
 
 echo "top ten female earners"
-cat $1 | grep -v 'gender' | sort -t , -k 4 -n | tail -n 10 | grep -c 'female'
+cat $1 | grep -v "gender" | sort -t , -k 4 -n | tail -n 10 | grep -c 'female'
 
 echo "effect of graduating college on mimimum wage"
 VAR1=$(cat $1 | grep -E '[female|male][,][0-9]{1,4}[,]16' | sort -t , -k 4 -n | head -n 1 | cut -d , -f 4)
